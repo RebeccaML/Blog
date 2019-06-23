451bdb5851f37de0a36885416b6a56d4ba7cc1a6
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-23 15:25:52 -0700

    added publish

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('about/', views.about, name='about'),
+    path('drafts/', views.drafts, name='drafts'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -18,7 +18,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -32,7 +31,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -41,3 +39,12 @@ def post_edit(request, pk):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+def drafts(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/drafts.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
