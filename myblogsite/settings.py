97aec234d4b5fa6a5def68e951cc22bcb29ab8d9
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-24 13:47:47 -0700

    database changed to postgresql

--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myblogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogsite',
+        'USER': 'Rebecca',
+        'PASSWORD': os.getenv('DATABASE_PASS'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
