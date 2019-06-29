4aa062ef0e04d92a9f0abeedd23aaea33263a0c8
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2019-06-29 11:16:34 -0700

    css

--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -131,7 +131,7 @@ LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'blog'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'blog'),)
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
